b3c43474f04a76b56f22610ec694996473df950e
RedsTom <tom.frefre.13@gmail.com>
2020-04-09 10:38:09 +0200

    Added @Plugin
    Signed-off-by: RedsTom <tom.frefre.13@gmail.com>

--- src/main/java/fr/synepixel/recipemanager/Main.java
+++ src/main/java/fr/synepixel/recipemanager/Main.java
@@ -1,15 +1,43 @@
 package fr.synepixel.recipemanager;
 
 import com.google.inject.Inject;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.slf4j.Logger;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartingServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
+import org.spongepowered.api.plugin.Plugin;
 
+import java.nio.file.Path;
+
+@Plugin(
+        name = "RecipesManager",
+        id = "recipes_manager",
+        version = "1.0",
+        description = "A plugin to create recipes",
+        authors = "RedsTom, Il_totore",
+        dependencies = {},
+        url = ""
+)
 public class Main {
 
     @Inject
     private Logger logger;
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private Path defaultConfig;
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private ConfigurationLoader<CommentedConfigurationNode> configManager;
+
+    @Inject
+    @ConfigDir(sharedRoot = true)
+    private Path privateConfigDir;
 
     @Listener
     public void onServerStart(GameStartingServerEvent event) {
--- src/main/java/fr/synepixel/recipemanager/recipes/RecipeManager.java
+++ src/main/java/fr/synepixel/recipemanager/recipes/RecipeManager.java
@@ -1,4 +1,16 @@
 package fr.synepixel.recipemanager.recipes;
 
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.item.recipe.crafting.Ingredient;
+
 public class RecipeManager {
+
+    public RecipeManager() {
+    }
+
+    public void addRecipe(Ingredient[] ingredients, ItemStack result) {
+
+
+    }
+
 }
